5cb42139c7b4f84dd2e30785b2a824b6a9bb646e
RuvimPiholyuk <rpiholyuk@gmail.com>
2022-04-19 16:48:42 -0700

    Update Blink_4_3.ino

--- Blink_1_8/Blink_4_3.ino
+++ Blink_1_8/Blink_4_3.ino
@@ -10,10 +10,12 @@ void setup() {
 
 // the loop function runs over and over again forever
 void loop() {
+  digitalWrite(2, HIGH);
   digitalWrite(10, HIGH);   // turn the external LED on (HIGH is the voltage level)
   digitalWrite(LED_BUILTIN, LOW);
+  digitalWrite(2, LOW);
+  delay(200);    // wait for a second
   digitalWrite(2, HIGH);
-  delay(200);                       // wait for a second
   digitalWrite(10, LOW);    // turn the LED off by making the voltage LOW
   digitalWrite(LED_BUILTIN, HIGH);
   digitalWrite(2, LOW);

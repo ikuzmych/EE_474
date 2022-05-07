e97dc3eb556c8a278fb90cd8d9dc1f6af9f50331
iluzmych <iluzmych@uw.edu>
2022-05-06 18:26:42 -0700

    Update lab2
    finished task1 ig

--- Lab2/Lab2.ino
+++ Lab2/Lab2.ino
@@ -1,12 +1,20 @@
 void setup() {
   // initialize pin outputs.
- pinMode(47, OUTPUT);
- pinMode(48, OUTPUT);
- pinMode(49, OUTPUT);
+
+  DDRL |= 1 << DDL2;
+  DDRL |= 1 << DDL1;
+  DDRL |= 1 << DDL0;
+  
+  /* pinMode(47, OUTPUT);
+     pinMode(48, OUTPUT);
+     pinMode(49, OUTPUT);
+  */
 }
 
-// the loop function runs over and over again forever
-void loop() {
+/* task 1 in lab2 
+ *  Simple light pattern utilizing digitalWrite()
+ */
+void lab2task1() {
     digitalWrite(47, HIGH);
     digitalWrite(49, LOW);
     delay(333);
@@ -15,5 +23,29 @@ void loop() {
     delay(333);
     digitalWrite(48, LOW);
     digitalWrite(49, HIGH);
+    delay(333);  
+}
+
+/* task2 in lab2 
+ *  Utilizes PORTL and bit-shifting
+ *  to turn on and off the LEDs in the right
+ *  order
+ */
+void lab2task2() {
+    PORTL |= 1 << PORTL2;
+    PORTL &= ~(1 << PORTL0);
     delay(333);
+    PORTL &= ~(1 << PORTL2);
+    PORTL |= 1 << PORTL1;
+    delay(333);
+    PORTL &= ~(1 << PORTL1);
+    PORTL |= 1 << PORT0;
+    delay(333);
+}
+
+
+// the loop function runs over and over again forever
+void loop() {
+  lab2task1();
+  lab2task2();
 }

2403aa5333c7d3917a4ad90463fcc4c8fdf9699c
iluzmych <iluzmych@uw.edu>
2022-05-21 14:48:53 -0700

    Update lab3.ino

--- lab3/lab3.ino
+++ lab3/lab3.ino
@@ -1,11 +1,33 @@
+#define NOTE_1 16000000 / 293 - 1
+#define NOTE_2 16000000 / 329 - 1
+#define NOTE_3 16000000 / 261 - 1
+#define NOTE_4 16000000 / 130 - 1
+#define NOTE_5 16000000 / 196 - 1
+#define NOTE_rest
+
+/* array defining all the frequencies of the melody  */
+int melody[] = { NOTE_1, NOTE_rest, NOTE_2, NOTE_rest, NOTE_3, NOTE_rest, NOTE_4, NOTE_rest, NOTE_5, NOTE_rest };
+int curr = 0;
+
 unsigned long timer;
 
 
 void setup() {
   DDRL |= 1 << DDL2;
+
+  noInterrupts();
+  TCCR4A = B01010100; // bottom two bits 0 (WGMn1 & WGMn0)
+  TCCR4B = B00001001; // 4th bit set to 1 (WGMn4 & WGMn3) and set bottom bit for clock select
+  DDRH |= 1 << DDH3; // pin 6
+  interrupts();
+  
 }
 
-void task1() {
+/** 
+ *  Simple pattern of turning on LED for 
+ *  250 ms, off for 750 ms, then repeating
+ */
+void task1(int on) {
   if (timer == 1) {
     PORTL |= 1 << PORTL2;
   } 
@@ -17,6 +39,25 @@ void task1() {
   }
   timer++;
   delay(1);
+}
+
+/**
+ * Playing a song for Task 2...
+ */
+void task2(int on) {
+ if (on) {
+    OCR4A = melody[curr % 10];
+    if (Time % 1000 == 0) {
+      curr++;
+    }
+    else if ((melody[curr % 10] == NOTE_rest) && (Time % 500 == 0)) {
+      curr++;
+    }
+  }
+  else {
+    OCR4A = 0;
+    curr = 0;
+  }
 }
 
 

13ddf2fd526e5f7822431a9220cbfa8ec4de00e3
iluzmych <iluzmych@uw.edu>
2022-05-21 15:13:57 -0700

    Update lab3.ino

--- lab3/lab3.ino
+++ lab3/lab3.ino
@@ -8,7 +8,7 @@
 /* array defining all the frequencies of the melody  */
 int melody[] = { NOTE_1, NOTE_rest, NOTE_2, NOTE_rest, NOTE_3, NOTE_rest, NOTE_4, NOTE_rest, NOTE_5, NOTE_rest };
 int curr = 0;
-
+int sleep;
 unsigned long timer;
 
 
@@ -45,19 +45,26 @@ void task1(int on) {
  * Playing a song for Task 2...
  */
 void task2(int on) {
- if (on) {
+ if (on && !sleep) {
     OCR4A = melody[curr % 10];
-    if ((timer % 1000 == 0) && (melody[curr % 10] != NOTE_rest)) {
+    if (timer % 1000 == 0 && !(melody[curr % 10] == NOTE_rest)) {
       curr++;
     }
     else if ((melody[curr % 10] == NOTE_rest) && (timer % 2000 == 0)) {
       curr++;
     }
+    if ( curr == 10 ) {
+      sleep = 1;
+      curr = 0;
+      OCR4A = 0;
+    }
   }
   else {
-    
     OCR4A = 0;
     curr = 0;
+  }
+  if (sleep == 1 && timer % 4000 == 0) {
+    sleep = 0;
   }
 }
 

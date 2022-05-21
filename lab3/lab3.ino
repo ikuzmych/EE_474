15d05efc5efb49a45b10eab7a4b9a5833ecb1940
RuvimPiholyuk <rpiholyuk@gmail.com>
2022-05-21 14:32:29 -0700

    New code part 1

--- lab3/lab3.ino
+++ lab3/lab3.ino
@@ -1,21 +1,22 @@
-unsigned long timer = 0;
+unsigned long timer;
 
 
 void setup() {
   DDRL |= 1 << DDL2;
-  
 }
 
 void task1() {
-  if (timer < 250) {
+  if (timer == 1) {
     PORTL |= 1 << PORTL2;
-  } else {
+  } 
+  if (timer == 250) {
     PORTL &= ~(1 << PORTL2);
   }
-  if (timer == 1000) {
+  if (timer > 1000) {
     timer = 0;
   }
   timer++;
+  delay(1);
 }
 
 

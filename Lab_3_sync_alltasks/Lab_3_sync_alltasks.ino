d0553cff3ad7f95544160c0aae06d4d0de4f62ed
iluzmych <iluzmych@uw.edu>
2022-05-25 15:50:40 -0700

    most stuff copied except few missing things

--- Lab_3_sync_alltasks/Lab_3_sync_alltasks.ino
+++ Lab_3_sync_alltasks/Lab_3_sync_alltasks.ino
@@ -4,7 +4,7 @@
 #define NOTE_4 16000000 / (2 * 130) - 1
 #define NOTE_5 16000000 / (2 * 196) - 1
 #define NOTE_rest 0
-#define N_TASKS 3
+#define N_TASKS 4
 #define READY 0
 #define RUNNING 1
 #define SLEEPING 2
@@ -13,6 +13,7 @@
 #define TICK 1
 
 #define DELAY_TIMEOUT_VALUE 16000000 / (2 * 500) - 1
+
 
 void (*taskPointers[N_TASKS]) (void);
 int state[N_TASKS];
@@ -55,10 +56,11 @@ unsigned long counter = 0;
 void setup() {
   taskPointers[0] = task1;
   taskPointers[1] = task2;
-  taskPointers[2] = NULL;
+  taskPointers[2] = task3;
+  taskPointers[3] = NULL;
   
-  // DDRA = 0b00011110;
-  // DDRC = 0xFF;
+  DDRA = 0b00011110;
+  DDRC = 0xFF;
   DDRL |= 1 << DDL2;
   
   noInterrupts();
@@ -148,6 +150,38 @@ void task2(void) {
   }
 }
 
+void task3 (void) {
+  if (// using the sleeping/resting logic) {
+    if (timer % 100 == 0) counter++;
+    
+    if (timer % 4 == 0) {
+      PORTC = 0b00000000;
+      PORTA = 0b11111101;
+      PORTC = seven_seg_digits[counter % 10];
+    }
+    else if (timer % 4 == 1) {
+      PORTC = 0b00000000;
+      PORTA = 0b11111011;
+      PORTC = seven_seg_digits[(counter / 10) % 10];
+      PORTC |= 0b00000001;
+    }
+    else if (timer % 4 == 2) {     
+      PORTC = 0b00000000;
+      PORTA = 0b11110111;
+      PORTC = seven_seg_digits[(counter / 100) % 10];
+    }
+    else if (timer % 4 == 3) {
+      PORTC = 0b00000000;
+      PORTA = 0b11101111;
+      PORTC = seven_seg_digits[(counter / 1000) % 10];
+    }
+  } else {
+    PORTA = 0b11111111;
+  }
+}
+
+
+
 void start_function(void (*functionPtr) () ) {
   functionPtr(); // runs the function that was passed down
   return;
@@ -163,7 +197,7 @@ void scheduler(void) {
   if ((taskPointers[task_index] == NULL) && (task_index != 0)) {
     task_index = 0;
   }
-  if (task_index > 3) { // just in case above one fails
+  if (task_index > NUM_TASKS) { // just in case above one fails
     task_index = 0;
   }
 //  if ((taskPointers[task_index] == NULL) && (task_index == 0)) {
